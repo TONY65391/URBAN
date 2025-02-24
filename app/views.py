bbbda8990cb7080f84dcbe780c19840eef35dd9d
TONY65391 <tonyanthony65391@gmail.com>
2025-02-24 08:00:07 -0800

    urbaneous

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.template import loader
+from django.contrib import messages
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(request = request))
+
+def images(request):
+    template = loader.get_template('images.html')
+    return HttpResponse(template.render(request = request))
